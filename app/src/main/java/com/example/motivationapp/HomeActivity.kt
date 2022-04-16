59fa5a4b741a584455e121b5fec1c02d09596915
GuerraAna <ana.guerrads@hotmail.com>
2022-04-16 03:08:19 -0300

    Feature: Implement view binding and sketch of logic about save message button click

--- app/src/main/java/com/example/motivationapp/HomeActivity.kt
+++ app/src/main/java/com/example/motivationapp/HomeActivity.kt
@@ -2,10 +2,26 @@ package com.example.motivationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivationapp.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSaveMessage.setOnClickListener {this}
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_save_message) {
+            val intent = ""
+        }
+    }
+
+
 }
