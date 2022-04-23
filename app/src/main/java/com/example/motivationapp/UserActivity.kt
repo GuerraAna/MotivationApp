cfc7fa1d28ad571da5d8f9e0d2f3faaa07236249
GuerraAna <ana.guerrads@hotmail.com>
2022-04-22 22:31:21 -0300

    Feature: Add base intent in UserActivity

--- app/src/main/java/com/example/motivationapp/UserActivity.kt
+++ app/src/main/java/com/example/motivationapp/UserActivity.kt
@@ -2,16 +2,24 @@ package com.example.motivationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.motivationapp.databinding.MotivapActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: MotivapActivityUserBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = MotivapActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_save_message) {
+            val intent = ""
+        }
     }
 }
